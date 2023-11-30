44c44f7f5fb5809a44057d1e1681fa9af1430862
nicolocamassa <camassanicolo03@gmail.com>
2023-11-30 18:16:41 +0100

    [FIX] start function with click fixed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
+let genBtn = document.getElementById('genBtn');
+
 function tableGen(bomb, BOMBS_NUM) {
+    console.log('cliccato');
+
     /* Scompare il bottone */
     btnNone = document.getElementById('noneDisplayed');
     btnNone.classList.add('d-none');
@@ -14,23 +18,23 @@ function tableGen(bomb, BOMBS_NUM) {
     /* Quantità dei quadrati in base alla difficoltà */
     console.log(difficulty)
 
-    switch(difficulty){
-        case 1: 
-        tableSize = 100;
-        container.style.width = '1000px'
-        break;
+    switch (difficulty) {
+        case 1:
+            tableSize = 100;
+            container.style.width = '1000px'
+            break;
 
         case 2:
-        tableSize = 81;
-        container.style.width = '900px'
-        break;
+            tableSize = 81;
+            container.style.width = '900px'
+            break;
 
         case 3:
-        tableSize = 49;
-        container.style.width = '700px'
-        break;
+            tableSize = 49;
+            container.style.width = '700px'
+            break;
     }
-        
+
 
     /* Generazione dei 100 quadrati */
     for (let i = 1; i <= tableSize; i++) {
@@ -45,34 +49,35 @@ function tableGen(bomb, BOMBS_NUM) {
         squareNum.innerHTML = i;
 
         /* Al click del quadrato viene cambiato il colore e mostrato in console la sua posizione */
+        clickedBomb = false;
         divGen.addEventListener('click', function () {
-            if(bomb.includes(parseInt(squareNum.innerHTML))){
-                console.log('Hai perso!')
-            }else{
-                this.classList.toggle('bg-primary')
-                console.log(parseInt(squareNum.innerHTML))     
-            }
             
+                if(bomb.includes(parseInt(squareNum.innerHTML))) {
+                    console.log('Hai perso!')
+                    bombClicked = true;
+                } else {
+                    this.classList.toggle('bg-primary')
+                    console.log(parseInt(squareNum.innerHTML))
+                }
         })
     }
+}
 
-}   
-
-function difficultyCheck(){
+function difficultyCheck() {
     let formValue = document.getElementById('difficulty').value;
     return formValue;
 }
 
-function generateBomb(BOMBS_NUM){
+function generateBomb(BOMBS_NUM) {
     /* Inizializzazione del vettore contenente le bombe */
     let bombPosition = [];
 
     /* Ciclo di generazione per le 16 bombe */
-    while(bombPosition.length != BOMBS_NUM){
-        let bombNumber = Math.floor(Math.random()* 100 +1 );
+    while (bombPosition.length != BOMBS_NUM) {
+        let bombNumber = Math.floor(Math.random() * 100 + 1);
 
         /* Controllo di numeri doppi nel vettore, se si verifica la condizione genera un altro numero*/
-        if(!bombPosition.includes(bombNumber)){
+        if (!bombPosition.includes(bombNumber)) {
             bombPosition.push(bombNumber);
         }
     }
@@ -82,11 +87,11 @@ function generateBomb(BOMBS_NUM){
 
 const BOMBS_NUM = 16;
 let bomb = generateBomb(BOMBS_NUM);
-let generateTable = tableGen(bomb, BOMBS_NUM);
 console.log(bomb);
 
-
-
+genBtn.addEventListener('click', function(){
+    tableGen(bomb, BOMBS_NUM);
+})
 
 
 
